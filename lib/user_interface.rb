e74fef00c7fbe063f915bf701327944e74f1f58b
MattW <mattnwarnock@yahoo.co.uk>
2020-11-21 15:58:08 +0000

    Pass tests for press any key to continue method

--- lib/user_interface.rb
+++ lib/user_interface.rb
@@ -3,12 +3,24 @@
 class UserInterface
   CLEAR_COMMAND = "\033[H\033[2J"
   INTRODUCTION_MESSAGE = 'Welcome to the quiz'
+  PRESS_ANY_KEY_MESSAGE = 'Press any key to continue '
 
-  def initialize(output)
+  def initialize(input, output)
+    @input = input
     @output = output
   end
 
   def intro
-    @output.print CLEAR_COMMAND, INTRODUCTION_MESSAGE
+    output.print CLEAR_COMMAND, INTRODUCTION_MESSAGE
+    continue
+  end
+
+  private
+
+  attr_reader :input, :output
+
+  def continue
+    output.print PRESS_ANY_KEY_MESSAGE
+    input.getch
   end
 end
--- spec/user_interface_spec.rb
+++ spec/user_interface_spec.rb
@@ -3,22 +3,41 @@
 require 'user_interface'
 
 RSpec.describe UserInterface do
+  let(:output) { StringIO.new }
+
   describe '#intro' do
+    let(:continue_input) { StringIO.new('x') }
+
     it 'clears the screen before printing message' do
-      output = StringIO.new
-      user_interface = described_class.new(output)
+      user_interface = described_class.new(continue_input, output)
 
       user_interface.intro
 
       expect(output.string).to include(described_class::CLEAR_COMMAND + described_class::INTRODUCTION_MESSAGE)
     end
+
     it 'prints an introduction to the screen' do
-      output = StringIO.new
-      user_interface = described_class.new(output)
+      user_interface = described_class.new(continue_input, output)
 
       user_interface.intro
 
       expect(output.string).to include(described_class::INTRODUCTION_MESSAGE)
+    end
+
+    it 'prompts user to press any key to continue' do
+      user_interface = described_class.new(continue_input, output)
+
+      user_interface.intro
+
+      expect(output.string).to include(described_class::PRESS_ANY_KEY_MESSAGE)
+    end
+
+    it 'returns key stroke entered by user' do
+      user_interface = described_class.new(continue_input, output)
+
+      result = user_interface.intro
+
+      expect(result).to eq('x')
     end
   end
 end
