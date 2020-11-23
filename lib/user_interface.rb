f7663c2fdb70092e74bcce38a56b5bdbe67ca8a3
MattW <mattnwarnock@yahoo.co.uk>
2020-11-23 14:02:09 +0000

    Setup test for display_results

--- lib/user_interface.rb
+++ lib/user_interface.rb
@@ -1,5 +1,7 @@
 # frozen_string_literal: true
 
+require 'colorize'
+
 class UserInterface
   CLEAR_COMMAND = "\033[H\033[2J"
   INTRODUCTION_MESSAGE = 'Welcome to the quiz'
--- spec/user_interface_spec.rb
+++ spec/user_interface_spec.rb
@@ -102,4 +102,63 @@ d\) 6
       expect(output.string.scan(described_class::ERROR_MESSAGE).length).to eq(2)
     end
   end
+
+  describe '#display_result' do
+    let(:input) { StringIO.new('x') }
+
+    it 'clears the screen before printing message' do
+      user_interface = described_class.new(input, output)
+
+      user_interface.display_result(described_class::FIRST_QUESTION)
+
+      expect(output.string).to include(described_class::CLEAR_COMMAND + described_class::FIRST_QUESTION[0])
+    end
+
+    it 'prints the question to the screen' do
+      user_interface = described_class.new(input, output)
+
+      user_interface.display_result(described_class::FIRST_QUESTION)
+
+      expect(output.string).to include(described_class::FIRST_QUESTION[0])
+    end
+
+    xit 'prints answer choices with the correct choice in blue text' do
+      user_interface = described_class.new(input, output)
+
+      user_interface.display_result(described_class::FIRST_QUESTION)
+
+      expect(output.string).to include(%(How many members are in Gojira?
+
+a\) 3
+b\) 5
+\e[34m c\) 4\e[0m
+d\) 6
+
+))
+    end
+
+    it 'prints result message to the screen' do
+      user_interface = described_class.new(input, output)
+
+      user_interface.display_result(described_class::FIRST_QUESTION)
+
+      expect(output.string).to include(described_class::CORRECT_MESSAGE)
+    end
+
+    it 'prompts user to press any key to continue' do
+      user_interface = described_class.new(input, output)
+
+      user_interface.display_result(described_class::FIRST_QUESTION)
+
+      expect(output.string).to include(described_class::PRESS_ANY_KEY_MESSAGE)
+    end
+
+    it 'returns key stroke entered by user' do
+      user_interface = described_class.new(input, output)
+
+      result = user_interface.display_result(described_class::FIRST_QUESTION)
+
+      expect(result).to eq('x')
+    end
+  end
 end
