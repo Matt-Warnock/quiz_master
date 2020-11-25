8e807e237f57c1c958aaed91db01effb74f1748c
MattW <mattnwarnock@yahoo.co.uk>
2020-11-25 13:53:11 +0000

    Create first 5 questions

--- lib/gojira_questions.rb
+++ lib/gojira_questions.rb
@@ -6,6 +6,12 @@ module GojiraQestions
   DECIMAL_COMPENSATOR = 97
   ERROR_MESSAGE = "I know what your playing at, you rebel! I'll only except a letter from a to "
   FIRST_QUESTION = ['How many members are in Gojira?', %w[3 5 4 6], 'c'].freeze
+  SECOND_QUESTION = ['Which city in France does the band originate from?', %w[Nantes Bayonne Lyon Paris], 'b'].freeze
+  THIRD_QUESTION = ["Gojira wasn't always known under that name, what was the original name of the band?",
+                    %w[Gorgonzola Oihuka Terra-Incognita Godzilla], 'd'].freeze
+  FORTH_QUESTION = ["What year was their debut album 'Terra-Incognita' released?", %w[2001 1996 2003 2016], 'a'].freeze
+  FIFTH_QUESTION = ["What album is the song 'Silvera' on?",
+                    ['The Link', "L'Enfant Sauvage", 'From Mars to Sirius', 'Magma'], 'd'].freeze
   INCORRECT_MESSAGE = 'Ah, thats the wrong answer.'
   INTRODUCTION_HEADER = %(
     ---------------------
