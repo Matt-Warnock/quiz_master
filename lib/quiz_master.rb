d3a390f11f034dade67930e8469bb844e06d3ef4
MattW <mattnwarnock@yahoo.co.uk>
2020-11-26 13:12:13 +0000

    Pass tests for intro and first question

--- lib/quiz_master.rb
+++ lib/quiz_master.rb
@@ -1,5 +1,7 @@
 # frozen_string_literal: true
 
+require 'gojira_questions'
+
 class QuizMaster
   def initialize(user_interface)
     @user_interface = user_interface
@@ -7,6 +9,9 @@ class QuizMaster
 
   def run
     user_interface.display_header
+    user_interface.intro(GojiraQestions::SUBJECT)
+    user_interface.display_header
+    user_interface.display_question(GojiraQestions::QUESTIONS[0][0])
   end
 
   private
--- spec/quiz_master_spec.rb
+++ spec/quiz_master_spec.rb
@@ -17,5 +17,49 @@ RSpec.describe QuizMaster do
 
       expect(output.string).to include(QuizMessages::CLEAR_COMMAND + QuizMessages::INTRODUCTION_HEADER)
     end
+
+    it 'prints introduction with subject of quiz' do
+      input = StringIO.new
+      user_interface = UserInterface.new(input, output)
+      controler = QuizMaster.new(user_interface)
+
+      controler.run
+
+      expect(output.string).to include(QuizMessages::INTRODUCTION_MESSAGE_ONE +
+                                       GojiraQestions::SUBJECT +
+                                       QuizMessages::INTRODUCTION_MESSAGE_TWO)
+    end
+
+    it 'asks user to press any key to continue after printing intro' do
+      input = StringIO.new
+      user_interface = UserInterface.new(input, output)
+      controler = QuizMaster.new(user_interface)
+
+      controler.run
+
+      expect(output.string).to include(QuizMessages::INTRODUCTION_MESSAGE_TWO + QuizMessages::PRESS_ANY_KEY_MESSAGE)
+    end
+
+    it 'clears screen then prints header before asking question' do
+      input = StringIO.new
+      user_interface = UserInterface.new(input, output)
+      controler = QuizMaster.new(user_interface)
+
+      controler.run
+
+      expect(output.string).to include(QuizMessages::CLEAR_COMMAND +
+                                       QuizMessages::INTRODUCTION_HEADER +
+                                       GojiraQestions::QUESTIONS[0][0])
+    end
+
+    it 'asks user first question' do
+      input = StringIO.new
+      user_interface = UserInterface.new(input, output)
+      controler = QuizMaster.new(user_interface)
+
+      controler.run
+
+      expect(output.string).to include(GojiraQestions::QUESTIONS[0][0])
+    end
   end
 end
