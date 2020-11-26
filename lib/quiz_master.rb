3a6b5daf8dfbdd149086470bdb4bde78a6e71df8
MattW <mattnwarnock@yahoo.co.uk>
2020-11-26 21:08:34 +0000

    Passed all test for question cycle and refactor

--- lib/quiz_master.rb
+++ lib/quiz_master.rb
@@ -4,17 +4,44 @@ require 'gojira_questions'
 
 class QuizMaster
   def initialize(user_interface)
-    @user_interface = user_interface
+    @ui = user_interface
   end
 
   def run
-    user_interface.display_header
-    user_interface.intro(GojiraQestions::SUBJECT)
-    user_interface.display_header
-    user_interface.display_question(GojiraQestions::QUESTIONS[0][0])
+    answer_choices = GojiraQestions::QUESTIONS[0][1]
+    correct_answer = GojiraQestions::QUESTIONS[0][2]
+
+    display_intro
+    user_answer = ask_question(GojiraQestions::QUESTIONS[0][0], answer_choices)
+    display_reveled_answer(answer_choices, correct_answer)
+    conclued_result(user_answer, correct_answer)
   end
 
   private
 
-  attr_reader :user_interface
+  def conclued_result(user_answer, correct_answer)
+    if user_answer == correct_answer
+      ui.display_result_message(QuizMessages::CORRECT_MESSAGE)
+    else
+      ui.display_result_message(QuizMessages::INCORRECT_MESSAGE)
+    end
+  end
+
+  def ask_question(question, answer_choices)
+    ui.display_header
+    ui.display_question(question)
+    ui.display_answer_choices(answer_choices)
+    ui.collect_valid_choice(answer_choices)
+  end
+
+  def display_reveled_answer(answer_choices, correct_answer)
+    ui.display_header
+    ui.reveal_answer(answer_choices, correct_answer)
+  end
+
+  def display_intro
+    ui.display_header
+    ui.intro(GojiraQestions::SUBJECT)
+  end
+  attr_reader :ui
 end
--- spec/quiz_master_spec.rb
+++ spec/quiz_master_spec.rb
@@ -7,9 +7,9 @@ require 'quiz_messages'
 RSpec.describe QuizMaster do
   describe '#run' do
     let(:output) { StringIO.new }
+    let(:input) { StringIO.new("\nb\n") }
 
     it 'clears the screen then prints header' do
-      input = StringIO.new
       user_interface = UserInterface.new(input, output)
       controler = QuizMaster.new(user_interface)
 
@@ -19,7 +19,6 @@ RSpec.describe QuizMaster do
     end
 
     it 'prints introduction with subject of quiz' do
-      input = StringIO.new
       user_interface = UserInterface.new(input, output)
       controler = QuizMaster.new(user_interface)
 
@@ -31,7 +30,6 @@ RSpec.describe QuizMaster do
     end
 
     it 'asks user to press any key to continue after printing intro' do
-      input = StringIO.new
       user_interface = UserInterface.new(input, output)
       controler = QuizMaster.new(user_interface)
 
@@ -41,7 +39,6 @@ RSpec.describe QuizMaster do
     end
 
     it 'clears screen then prints header before asking question' do
-      input = StringIO.new
       user_interface = UserInterface.new(input, output)
       controler = QuizMaster.new(user_interface)
 
@@ -53,13 +50,55 @@ RSpec.describe QuizMaster do
     end
 
     it 'asks user first question' do
-      input = StringIO.new
       user_interface = UserInterface.new(input, output)
       controler = QuizMaster.new(user_interface)
 
       controler.run
 
       expect(output.string).to include(GojiraQestions::QUESTIONS[0][0])
+    end
+
+    it 'displays answer choices' do
+      user_interface = UserInterface.new(input, output)
+      controler = QuizMaster.new(user_interface)
+
+      controler.run
+
+      expect(output.string).to include(GojiraQestions::QUESTIONS[0][1][0],
+                                       GojiraQestions::QUESTIONS[0][1][1],
+                                       GojiraQestions::QUESTIONS[0][1][2],
+                                       GojiraQestions::QUESTIONS[0][1][3])
+    end
+
+    it 'clears screen then prints header before reveling correct answer' do
+      user_interface = UserInterface.new(input, output)
+      controler = QuizMaster.new(user_interface)
+
+      controler.run
+
+      expect(output.string).to include(QuizMessages::CLEAR_COMMAND +
+                                       QuizMessages::INTRODUCTION_HEADER,
+                                       "\e[0;34;49mc) #{GojiraQestions::QUESTIONS[0][1][2]}\e[0m")
+    end
+
+    it 'displays a congratulatory message if user answer is correct' do
+      correct_answer_input = StringIO.new("\nc\n")
+      user_interface = UserInterface.new(correct_answer_input, output)
+      controler = QuizMaster.new(user_interface)
+
+      controler.run
+
+      expect(output.string).to include(QuizMessages::CORRECT_MESSAGE)
+    end
+
+    it 'displays a consolatory message if user answer is incorrect' do
+      correct_answer_input = StringIO.new("\nd\n")
+      user_interface = UserInterface.new(correct_answer_input, output)
+      controler = QuizMaster.new(user_interface)
+
+      controler.run
+
+      expect(output.string).to include(QuizMessages::INCORRECT_MESSAGE)
     end
   end
 end
